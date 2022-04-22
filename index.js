76eac4b26247b8c85eda486664f78705032a3686
Tony <almanaxstories@gmail.com>
2022-04-22 22:08:51 +0300

    Task is solved. Conditions of problem is fullfield. Wepack, eslint, prettier are NOT installed yet

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ window.onload = () => {
 
 
     let taskManagerMainDiv = document.getElementById('container');
+
+    let divForStartMessage = document.createElement('div');
+    taskManagerMainDiv.appendChild(divForStartMessage);
+
+    let startMessageFirstBlock = document.createElement('h2');
+    startMessageFirstBlock .textContent = 'Hi User, this is your personal task manager';
+    startMessageFirstBlock .style = 'text-align: center; color: violet;';
+    divForStartMessage.appendChild(startMessageFirstBlock);
+
+    let startMessageSecondBlock = document.createElement('h3');
+    startMessageSecondBlock.textContent = 'Print your tasks in the field below one by one and push adding button';
+    startMessageSecondBlock.style = 'text-align: center; color: orange;';
+    divForStartMessage.appendChild(startMessageSecondBlock);
+
+    let startMessageThirdBlock = document.createElement('h4');
+    startMessageThirdBlock.textContent = 'Pushing adding button with empty string doing nothing';
+    startMessageThirdBlock.style = 'text-align: center; color: red;';
+    divForStartMessage.appendChild(startMessageThirdBlock);
     
     let divForInputAndButton = document.createElement('div');
     divForInputAndButton.style = 'margin:1%;';
@@ -19,10 +37,17 @@ window.onload = () => {
 
     let addTaskButton = document.createElement('input');
     addTaskButton.type = 'button';
-    addTaskButton.style = 'width: 140px; height: 23px; margin-left: 5%';
+    addTaskButton.style = 'width: 100px; height: 23px; margin-left: 1%';
     addTaskButton.value = 'Add New Task';
     addTaskButton.addEventListener('click', addNewTask);
     divForInputAndButton.appendChild(addTaskButton);
+
+    let addCLearButton = document.createElement('input');
+    addCLearButton.type = 'button';
+    addCLearButton.style = 'width: 80px; height: 23px; margin-left: 1%;';
+    addCLearButton.value = 'Clear Tasks';
+    addCLearButton.addEventListener('click', clearTaskManagerSpace);
+    divForInputAndButton.appendChild(addCLearButton);
     
     let divForUl = document.createElement('div');
     divForUl.style = 'margin-top: 3%';
@@ -31,32 +56,51 @@ window.onload = () => {
     let mainUl = document.createElement('ul');
     divForUl.appendChild(mainUl);
 
-    let arrLiKeeper = [];
+    let arrayOfLiElements = [];
+
+    generateLiElementsFromLocalStorage();
 
     function addNewTask(){
         if(taskInputField.value === ''){
             return undefined;
         }
 
-            arrLiKeeper.push(taskInputField.value);
-            arrLiKeeper.sort();
+            arrayOfLiElements.push(taskInputField.value);
+            arrayOfLiElements.sort();
             mainUl.innerHTML = '';
-            for(let a in arrLiKeeper){
-                createAndAddNewLi(arrLiKeeper[a]);
+
+            for(let a in arrayOfLiElements){
+                createAndAddNewLi(arrayOfLiElements[a]);
             }
-        
-        
-        function createAndAddNewLi(liElementTextContent){
+
+            
+            let stringifiedArrayForLocalStorage = JSON.stringify(arrayOfLiElements);
+            localStorage.setItem('tasks', stringifiedArrayForLocalStorage);
+
+    }
+
+    function createAndAddNewLi(liElementTextContent){
         let newLi = document.createElement('li');
         newLi.textContent = liElementTextContent;
         mainUl.appendChild(newLi);
         taskInputField.value = '';
         }
+
+
+    function generateLiElementsFromLocalStorage(){
+        if(!localStorage.getItem('tasks')){
+            return undefined;
+        }
+
+        arrayOfLiElements = JSON.parse(localStorage.getItem('tasks'));
+        
+        for(let a of arrayOfLiElements){
+            createAndAddNewLi(a);
+        }   
     }
 
-
-
-
-
-
+    function clearTaskManagerSpace(){
+        localStorage.clear();
+        mainUl.innerHTML = '';
+    }
 }
